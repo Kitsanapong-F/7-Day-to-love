3c90604a0fa20156f8630a60f929217300495763
Kitsanapong-F <09chaisu@gmail.com>
2026-02-27 22:29:32 +0700

    add settiScene

--- StartGame.java
+++ StartGame.java
@@ -33,6 +33,8 @@ public class StartGame extends BaseFrame {
         
 
         settingBtn.addActionListener(e -> {
+            AudioManager.playSound("umamusume_con.wav");
+            SceneManager.switchScene(new SettingScene()); 
         });
         
 
